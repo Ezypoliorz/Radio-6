3c633c19ee570f0a88f8bd775d853a1f518479b4
Ezypoliorz <ezypoliorz.275@gmail.com>
2025-02-06 06:41:22 +0100

    v2.0

---
+++ script-podcasts.js
@@ -0,0 +1,58 @@
+const draggable = document.getElementById("Curseur");
+let isDragging = false;
+let offsetY = 0;
+let positionY = 0;
+let son = 0;
+let YindexMin = 200;
+let YindexMax = 415;
+
+draggable.addEventListener('mousedown', (event) => {
+    
+    isDragging = true;
+    offsetY = event.clientY - draggable.offsetTop;
+    volume = volume + offsetY;
+    
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+    document.getElementById("Curseur").style.cursor = 'grab';
+});
+
+document.addEventListener('mousemove', (event) => {
+    
+    if (isDragging) {
+        if (event.clientY - offsetY >= YindexMin && event.clientY - offsetY <= YindexMax) {
+        document.getElementById("Curseur").style.cursor = 'grabbing';
+        positionY = event.clientY;
+        draggable.style.top = positionY - offsetY + 'px';
+        son = event.clientY - offsetY;
+        son = Number(son);
+        son = (son - YindexMin) / (YindexMax - YindexMin)
+        son = 1 - son;
+        son = son.toFixed(2);
+        if (son > 0.6) {
+            document.getElementById("Volume-icone-img").src = "Images/Volume ON.svg"
+            const allAudios = document.querySelectorAll('audio');
+            allAudios.forEach(audio => {
+                audio.volume = son;
+            });
+        }
+        if (son <= 0.6) {
+            document.getElementById("Volume-icone-img").src = "Images/Volume MILIEU.svg"
+            const allAudios = document.querySelectorAll('audio');
+            allAudios.forEach(audio => {
+                audio.volume = son;
+            });
+        }
+        if (son <= 0.03) {
+            document.getElementById("Volume-icone-img").src = "Images/Volume OFF.svg"
+            const allAudios = document.querySelectorAll('audio');
+            allAudios.forEach(audio => {
+                audio.volume = 0;
+            });
+            
+        }
+    }
+    }
+});
