3425f82c42dcb498733e439e24a47f0498f275ab
Ezypoliorz <ezypoliorz.275@gmail.com>
2025-02-28 14:51:19 +0100

    v3.1.5

--- script-émissions.js
+++ script-émissions.js
@@ -1,50 +1,33 @@
-document.getElementById("Programme-background1").style.display = "none";
-document.getElementById("Programme-background2").style.display = "none";
-document.getElementById("Programme-background3").style.display = "none";
-document.getElementById("Programme-background4").style.display = "none";
-document.getElementById("Programme-background5").style.display = "none";
+div_programme_background = document.querySelectorAll(".div-programme-background"); // On récupère tous les éléments de classe "div-programme-background"
+div_programme_background.forEach(element => { // Pour chaque élément de la liste
 
-document.getElementById("Close1").onclick = function() {
-  document.getElementById("Programme-background1").style.display = "none";
+  element.style.display = "none"; // On cache l'élément
 
-};
-document.getElementById("Close2").onclick = function() {
-  document.getElementById("Programme-background2").style.display = "none";
+});
 
-};
-document.getElementById("Close3").onclick = function() {
-  document.getElementById("Programme-background3").style.display = "none";
+fermer = document.getElementsByClassName("fermer"); // On récupère tous les éléments de classe "fermer"
+for (let i = 0; i < fermer.length; i++) { // Pour chaque élément de la liste
 
-};
-document.getElementById("Close4").onclick = function() {
-  document.getElementById("Programme-background4").style.display = "none";
+  let id_self = fermer[i].id; // On récupère l'id de l'élément
+  let id_self_number = id_self.match(/\d+/)[0]; // On récupère le nombre de l'id
+  let id_target = "Programme-background" + (id_self_number); // On crée l'id de l'élément cible
+  document.getElementById(id_self).onclick = function() { // On crée un événement au clic
+  document.getElementById(id_target).style.display = "none"; // On cache l'élément cible
+  }
 
-};
-document.getElementById("Close5").onclick = function() {
-  document.getElementById("Programme-background5").style.display = "none";
+}
 
-};
+infos = document.getElementsByClassName("infos"); // On récupère tous les éléments de classe "infos"
+for (let i = 0; i < infos.length; i++) { // Pour chaque élément de la liste
 
-document.getElementById("Infos1").onclick = function() {
-  document.getElementById("Programme-background1").style.display = "block";
+  let id_self = infos[i].id; // On récupère l'id de l'élément
+  let id_self_number = id_self.match(/\d+/)[0]; // On récupère le nombre de l'id
+  let id_target = "Programme-background" + (id_self_number); // On crée l'id de l'élément cible
+  document.getElementById(id_self).onclick = function() { // On crée un événement au clic
+  document.getElementById(id_target).style.display = "block"; // On affiche l'élément cible
+  }
 
-};
-document.getElementById("Infos2").onclick = function() {
-  document.getElementById("Programme-background2").style.display = "block";
-
-};
-document.getElementById("Infos3").onclick = function() {
-  document.getElementById("Programme-background3").style.display = "block";
-
-};
-document.getElementById("Infos4").onclick = function() {
-  document.getElementById("Programme-background4").style.display = "block";
-
-};
-document.getElementById("Infos5").onclick = function() {
-  document.getElementById("Programme-background5").style.display = "block";
-
-};
+}
 
 const draggable = document.getElementById("Curseur");
 let isDragging = false;
@@ -54,47 +37,47 @@ let son = 0;
 let YindexMin = 200;
 let YindexMax = 415;
 
-draggable.addEventListener('mousedown', (event) => {
+draggable.addEventListener('mousedown', (event) => { // On crée un événement au clic
     
-    isDragging = true;
-    offsetY = event.clientY - draggable.offsetTop;
-    volume = volume + offsetY;
+    isDragging = true; 
+    offsetY = event.clientY - draggable.offsetTop; // On récupère la position y du curseur
+    volume = volume + offsetY; // On ajoute la position y du curseur à la variable volume
     
 });
 
-document.addEventListener('mouseup', () => {
+document.addEventListener('mouseup', () => { // On crée un événement au relâchement du clic
     isDragging = false;
-    document.getElementById("Curseur").style.cursor = 'grab';
+    document.getElementById("Curseur").style.cursor = 'grab'; // On change le curseur
 });
 
-document.addEventListener('mousemove', (event) => {
+document.addEventListener('mousemove', (event) => { // On crée un événement au déplacement de la souris
     
-    if (isDragging) {
-        if (event.clientY - offsetY >= YindexMin && event.clientY - offsetY <= YindexMax) {
+    if (isDragging) { // Si on clique
+        if (event.clientY - offsetY >= YindexMin && event.clientY - offsetY <= YindexMax) { // Si la position y du curseur est comprise entre 200 et 415
         document.getElementById("Curseur").style.cursor = 'grabbing';
-        positionY = event.clientY;
-        draggable.style.top = positionY - offsetY + 'px';
+        positionY = event.clientY; // On récupère la position y de la souris
+        draggable.style.top = positionY - offsetY + 'px'; 
         son = event.clientY - offsetY;
         son = Number(son);
-        son = (son - YindexMin) / (YindexMax - YindexMin)
+        son = (son - YindexMin) / (YindexMax - YindexMin); // On calcule le volume
         son = 1 - son;
-        son = son.toFixed(2);
-        if (son > 0.6) {
-            document.getElementById("Volume-icone-img").src = "Images/Volume ON.svg"
+        son = son.toFixed(2); // On arrondit le volume à 2 chiffres après la virgule
+        if (son > 0.6) { // Si le volume est supérieur à 0.6
+            document.getElementById("Volume-icone-img").src = "Images/Volume ON.svg" // On change l'image de l'icône du volume
             const allAudios = document.querySelectorAll('audio');
             allAudios.forEach(audio => {
                 audio.volume = son;
             });
         }
-        if (son <= 0.6) {
-            document.getElementById("Volume-icone-img").src = "Images/Volume MILIEU.svg"
+        if (son <= 0.6) { // Si le volume est inférieur ou égal à 0.6
+            document.getElementById("Volume-icone-img").src = "Images/Volume MILIEU.svg" // On change l'image de l'icône du volume
             const allAudios = document.querySelectorAll('audio');
             allAudios.forEach(audio => {
                 audio.volume = son;
             });
         }
-        if (son <= 0.03) {
-            document.getElementById("Volume-icone-img").src = "Images/Volume OFF.svg"
+        if (son <= 0.03) { // Si le volume est inférieur ou égal à 0.03
+            document.getElementById("Volume-icone-img").src = "Images/Volume OFF.svg"  // On change l'image de l'icône du volume
             const allAudios = document.querySelectorAll('audio');
             allAudios.forEach(audio => {
                 audio.volume = 0;
@@ -107,23 +90,23 @@ document.addEventListener('mousemove', (event) => {
 
 
 function pauseOtherAudios(audioElement) {
-const allAudios = document.querySelectorAll('audio');
-allAudios.forEach(audio => {
-  if (audio !== audioElement) {
-    audio.pause();
+const allAudios = document.querySelectorAll('audio'); // On récupère tous les éléments audio
+allAudios.forEach(audio => { // Pour chaque élément audio
+  if (audio !== audioElement) { // Si l'élément audio est différent de l'élément audio actuel
+    audio.pause(); // On met en pause l'élément audio
   }
 });
 }
 
-const audios = document.querySelectorAll('audio');
-audios.forEach(audio => {
-audio.addEventListener('play', () => {
-  pauseOtherAudios(audio);
+const audios = document.querySelectorAll('audio'); // On récupère tous les éléments audio
+audios.forEach(audio => { // Pour chaque élément audio
+audio.addEventListener('play', () => { // On crée un événement au lancement de la lecture
+  pauseOtherAudios(audio); // On met en pause les autres éléments audio
 });
 });
 
-if (navigator.userAgent.match(/iPad|Android|Tablet/i)) {
-  document.getElementById("Niveau-sonore").style.display = "none";
+if (navigator.userAgent.match(/iPad|Android|Tablet/i)) { // Si l'utilisateur est sur un iPad, un Android ou une tablette
+  document.getElementById("Niveau-sonore").style.display = "none"; // On cache l'élément "Niveau-sonore"
 }
 
 document.getElementById("Bouton-nav-téléphone").onclick = function() {
