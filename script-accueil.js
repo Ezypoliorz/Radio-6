8f6e76f90dfd5bd9e35676ba80e8b5d62f4553e7
Ezypoliorz <ezypoliorz.275@gmail.com>
2025-03-27 13:14:52 +0100

    Upload automatique

--- script-accueil.js
+++ script-accueil.js
@@ -1,8 +1,3 @@
-if (navigator.userAgent.match(/iPad|Android|Tablet/i)) { // Si l'utilisateur est sur un iPad, un Android ou une tablette
-  document.getElementById("Niveau-sonore").style.display = "none"; // On cache l'élément "Niveau-sonore"
-
-}
-
 document.getElementById("Bouton-nav-téléphone").onclick = function() {
 
   document.getElementById("Div-nav-téléphone").style.display = "block";
