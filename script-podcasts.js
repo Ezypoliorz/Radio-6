73bbcd78d8ae5c87b1e3017a9f997b6937623820
Ezypoliorz <ezypoliorz.275@gmail.com>
2025-04-01 10:40:17 +0200

    Upload automatique

--- script-podcasts.js
+++ script-podcasts.js
@@ -58,8 +58,13 @@ document.addEventListener('mousemove', (event) => {
 });
 
 
-document.getElementById("Bouton-nav-téléphone").onclick = function() {
-
+if (navigator.userAgent.match(/iPad|Android|Tablet/i)) { // Si l'utilisateur est sur un iPad, un Android ou une tablette
+    document.getElementById("Niveau-sonore").style.display = "none"; // On cache l'élément "Niveau-sonore"
+  
+  }
+  
+  document.getElementById("Bouton-nav-téléphone").onclick = function() {
+  
     document.getElementById("Div-nav-téléphone").style.display = "block";
     document.getElementById("Conteneur").style.display = "none";
     document.getElementById("Bouton-nav-téléphone").style.display = "none";
